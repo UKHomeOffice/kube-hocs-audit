6bf9adadbc04cff4b2a608d205cec869ba17f509
Paul Rau <paul.rau1@digital.homeoffice.gov.uk>
2020-05-29 16:28:49 +0100

    HOCS-1751 Updating for qa and prod deployments. The namespace is now passed through the environment on the command.

--- deploy.sh
+++ deploy.sh
@@ -1,32 +1,46 @@
 #!/bin/bash
 
-export KUBE_NAMESPACE=${KUBE_NAMESPACE}
+export KUBE_NAMESPACE=${ENVIRONMENT}
 export KUBE_SERVER=${KUBE_SERVER}
 
 if [[ -z ${VERSION} ]] ; then
     export VERSION=${IMAGE_VERSION}
 fi
 
-if [[ ${ENVIRONMENT} == "prod" ]] ; then
-    echo "deploy ${VERSION} to PROD namespace, using HOCS_AUDIT_PROD drone secret"
-    export KUBE_TOKEN=${HOCS_AUDIT_PROD}
+if [[ ${ENVIRONMENT} == "cs-prod" ]] ; then
+    echo "deploy ${VERSION} to PROD namespace, using HOCS_AUDIT_CS_PROD drone secret"
+    export KUBE_TOKEN=${HOCS_AUDIT_CS_PROD}
     export REPLICAS="2"
+elif [[ ${ENVIRONMENT} == "wcs-prod" ]] ; then
+    echo "deploy ${VERSION} to PROD namespace, using HOCS_AUDIT_WCS_PROD drone secret"
+    export KUBE_TOKEN=${HOCS_AUDIT_WCS_PROD}
+    export REPLICAS="2"
+elif [[ ${ENVIRONMENT} == "cs-qa" ]] ; then
+    echo "deploy ${VERSION} to QA namespace, using HOCS_AUDIT_CS_QA drone secret"
+    export KUBE_TOKEN=${HOCS_AUDIT_CS_QA}
+    export REPLICAS="2"
+elif [[ ${ENVIRONMENT} == "wcs-qa" ]] ; then
+    echo "deploy ${VERSION} to QA namespace, using HOCS_AUDIT_WCS_QA drone secret"
+    export KUBE_TOKEN=${HOCS_AUDIT_WCS_QA}
+    export REPLICAS="2"
+elif [[ ${ENVIRONMENT} == "cs-demo" ]] ; then
+    echo "deploy ${VERSION} to DEMO namespace, using HOCS_AUDIT_CS_DEMO drone secret"
+    export KUBE_TOKEN=${HOCS_AUDIT_CS_DEMO}
+    export REPLICAS="1"
+elif [[ ${ENVIRONMENT} == "wcs-demo" ]] ; then
+    echo "deploy ${VERSION} to DEMO namespace, using HOCS_AUDIT_WCS_DEMO drone secret"
+    export KUBE_TOKEN=${HOCS_AUDIT_WCS_DEMO}
+    export REPLICAS="1"
+elif [[ ${ENVIRONMENT} == "cs-dev" ]] ; then
+    echo "deploy ${VERSION} to DEV namespace, using HOCS_AUDIT_CS_DEV drone secret"
+    export KUBE_TOKEN=${HOCS_AUDIT_CS_DEV}
+    export REPLICAS="1"
+elif [[ ${ENVIRONMENT} == "wcs-dev" ]] ; then
+    echo "deploy ${VERSION} to DEV namespace, using HOCS_AUDIT_WCS_DEV drone secret"
+    export KUBE_TOKEN=${HOCS_AUDIT_WCS_DEV}
+    export REPLICAS="1"
 else
-    if [[ ${ENVIRONMENT} == "qa" ]] ; then
-        echo "deploy ${VERSION} to QA namespace, using HOCS_AUDIT_QA drone secret"
-        export KUBE_TOKEN=${HOCS_AUDIT_QA}
-        export REPLICAS="2"
-    elif [[ ${ENVIRONMENT} == "demo" ]] ; then
-        echo "deploy ${VERSION} to DEMO namespace, using HOCS_AUDIT_DEMO drone secret"
-        export KUBE_TOKEN=${HOCS_AUDIT_DEMO}
-        export REPLICAS="1"
-    elif [[ ${ENVIRONMENT} == "dev" ]] ; then
-        echo "deploy ${VERSION} to DEV namespace, using HOCS_AUDIT_DEV drone secret"
-        export KUBE_TOKEN=${HOCS_AUDIT_DEV}
-        export REPLICAS="1"        
-    else
-        echo "Unable to find environment: ${ENVIRONMENT}"
-    fi
+    echo "Unable to find environment: ${ENVIRONMENT}"
 fi
 
 if [[ -z ${KUBE_TOKEN} ]] ; then
@@ -34,14 +48,14 @@ if [[ -z ${KUBE_TOKEN} ]] ; then
     exit -1
 fi
 
-if [[ "${KUBE_NAMESPACE}" == "wcs-prod" ]] ; then
+if [[ "${ENVIRONMENT}" == "wcs-prod" ]] ; then
     export DNS_PREFIX=www.${DOMAIN}
     export KC_REALM=https://sso.digital.homeoffice.gov.uk/auth/realms/HOCS
-elif [[ "${ENVIRONMENT}" == "prod" ]] ; then
+elif [[ "${ENVIRONMENT}" == "cs-prod" ]] ; then
     export DNS_PREFIX=www.${DOMAIN}
     export KC_REALM=https://sso.digital.homeoffice.gov.uk/auth/realms/hocs-prod
 else
-    export DNS_PREFIX=${ENVIRONMENT}.${DOMAIN}-notprod
+    export DNS_PREFIX=${DOMAIN}.${DOMAIN}-notprod
     export KC_REALM=https://sso-dev.notprod.homeoffice.gov.uk/auth/realms/hocs-notprod
 fi
 
